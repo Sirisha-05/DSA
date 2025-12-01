f88b975934f22b650b019d53d19dd7be07cc096e
Kancherla Sirisha <kancherlasirisha3@gmail.com>
2025-12-01 09:01:34 +0530

    Time: 1 ms (16.89%) | Memory: 48.1 MB (18.21%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,20 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
-       int p1=firstOcurence(nums,n,target);
-        int p2=lastOccurence(nums,n,target);
-        //typecast to array and return them
-        return new int[]{p1,p2};
-    }
-    //calculate first occurence and return the index 
-    public static int firstOcurence(int a[],int n,int b){
-        int l=0,h=n-1;
-        int res=-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(a[mid]==b){
-            res=mid;
-            h=mid-1;
-            }
-          else   if(a[mid]<b){
-            l=mid+1;
-            }
-          else  if(a[mid]>b){
-            h=mid-1;
+        int[] a=new int[2];
+        a[0]=-1;
+        a[1]=-1;
+        for(int i=0;i<nums.length;i++){
+            if(a[0]==-1&&nums[i]==target){
+                a[0]=i;
+                break;
             }
         }
-        return res;
-    }
-    //calculate last occurence and return index
-    public static int lastOccurence(int a[],int n,int b){
-        int l=0;
-        int h=n-1;
-        int res=-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(a[mid]==b){
-            res=mid;
-            l=mid+1;
-            }
-          else  if(a[mid]<b){
-            l=mid+1;
-            }
-          else  if(a[mid]>b){
-            h=mid-1;
+        for(int i=nums.length-1;i>=0;i--){
+            if(a[1]==-1&&nums[i]==target){
+                a[1]=i;
+                break;
             }
         }
-        return res;
+        return a;
     }
 }
